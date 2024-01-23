0c3bf8c30507018357c6961b06e0360c6c1be9a6
Christian Georgeot <christian.georgeot@gmail.com>
2024-01-23 06:57:29 +0100

    Positionnement et style du formulaire de contact

--- template-parts/popup-contact.php
+++ template-parts/popup-contact.php
@@ -1,7 +1,6 @@
 <div id="popup-contact" class="modal">
 
   <div class="modal-content">
-
     <?php 
     echo do_shortcode('[contact-form-7 id="32bd04f" title="Formulaire de contact MotaPhoto"]') 
     ?>
