4baa3299054d3ba73e20ebc20881b846e4481646
Christian Georgeot <christian.georgeot@gmail.com>
2024-02-06 14:53:31 +0100

    Bloc photo stylage minimal

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -1,34 +1,22 @@
 <?php
 /**
  * Bloc photo 
- * Le hero header avec une image en haute qualité prise au
- * hasard parmi celles de la photothèque de Nathalie Mota.
- * La mention "PHOTOGRAPHE EVENT" en lettres détourées est
- * superposé au centre via le code CSS pour être responsive.
-*/
+ */
 ?>
 
-<div>
+<div class="photo-container">
+    <?php if (has_post_thumbnail()) { the_post_thumbnail('medium'); } ?>
+    <div class="photo-overlay">
+        <span class="reference">
+            <?php echo get_field_object('field_65af94c95d70a')['value']; ?>
+        </span>
+        <span class="categories">
+            <?php echo implode(' ',  wp_get_post_terms($post->ID, 'categorie', ['fields' => 'names'])); ?>
+        </span>
+        <a class="single-link" href="<?php echo get_post_permalink(); ?>"></a>
+        <button class="lightbox-btn" onclick="ShowInLightbox(this)" data-postid="<?php echo get_the_ID(); ?>">
+            Lightbox
+        </button>
+    </div>
 </div>
-<?php
-
-if (has_post_thumbnail()) {
-    the_post_thumbnail('medium');
-}
-
-echo '<br>';
-echo get_field_object('field_65af94c95d70a')['value'];
-echo ' ';
-echo implode(' ',  wp_get_post_terms($post->ID, 'categorie', ['fields' => 'names']));
-echo ' ';
-?>
-<span>
-    <a href="<?php echo get_post_permalink(); ?>">Oeil</a>
-</span>
-<button onclick="ShowInLightbox(this)" data-postid="<?php echo get_the_ID(); ?>">
-    Lightbox
-</button>
-<?php
-
-?>
 
