7f3f06e46c05ebcbe5d226dfb3cce4eb4a2ada80
Christian Georgeot <christian.georgeot@gmail.com>
2024-02-10 08:19:37 +0100

    Style sur zone de filtre et de tri de la page d'accueil

--- template-parts/filter-sort-form.php
+++ template-parts/filter-sort-form.php
@@ -12,20 +12,24 @@
     <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('request_filtered_photos'); ?>">
     <input type="hidden" name="action" value="request_filtered_photos">
 
-    <?php foreach (get_object_taxonomies('photo', 'object') as $tax) : ?>
-        <select id="filtre-<?php echo $tax->name ?>">
-            <option value="*" selected><?php echo $tax->labels->name ?></option>
-            <?php foreach(get_terms(['taxonomy' => $tax->name]) as $choice) : ?>
-            <option value="<?php echo $choice->slug ?>"><?php echo $choice->name ?></option>
-            <?php endforeach ?>
-        </select>
-    <?php endforeach ?>
+    <div class="zone-filtre">
+        <?php foreach (get_object_taxonomies('photo', 'object') as $tax) : ?>
+            <select id="filtre-<?php echo $tax->name ?>">
+                <option value="*" selected><?php echo $tax->labels->name ?></option>
+                <?php foreach(get_terms(['taxonomy' => $tax->name]) as $choice) : ?>
+                <option value="<?php echo $choice->slug ?>"><?php echo $choice->name ?></option>
+                <?php endforeach ?>
+            </select>
+        <?php endforeach ?>
+    </div>
 
-    <select id="ordre-tri">
-        <option disabled selected><?php _e('Trier par', 'motaphoto'); ?></option>
-        <option value="DESC"><?php echo _e('À partir des plus récentes'); ?></option>
-        <option value="ASC"><?php echo _e('À partir des plus anciennes'); ?></option>
-    </select>
+    <div class="zone-tri">
+        <select id="ordre-tri">
+            <option disabled selected><?php _e('Trier par', 'motaphoto'); ?></option>
+            <option value="DESC"><?php echo _e('À partir des plus récentes'); ?></option>
+            <option value="ASC"><?php echo _e('À partir des plus anciennes'); ?></option>
+        </select>
+    </div>
 
 </form>
 
